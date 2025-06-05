833e667f4f5b2563c8293f16f5faf084f9c330b0
MINETA "m10i" Hiroki <h-mineta@0nyx.net>
2025-06-06 06:46:36 +0900

    [modify] Update 4.Start_ComfyUI.bat

--- 4.Start_ComfyUI.bat
+++ 4.Start_ComfyUI.bat
@@ -1,1 +1,7 @@
 wsl.exe -d ComfyUI-Fedora -- bash -c "/opt/ComfyUI-running-on-Podman-WSL2/start_comfyui.sh"
+
+REM Sleep for 5 seconds to start
+timeout /t 5 /nobreak >nul
+
+REM Open ComfyUI in the default web browser
+start "" "http://localhost:8888" >nul 2>&1
